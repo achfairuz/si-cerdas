7a29ad9bba0009099c00fe93d3edad3ec5567339
Achmad Fairuz <159877779+achfairuz@users.noreply.github.com>
2026-02-15 20:51:13 +0700

    fix: app proovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Category;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Paginator::useTailwind();
         View::composer('*', function ($view) {
 
